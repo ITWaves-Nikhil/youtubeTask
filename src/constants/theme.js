3493a38a6902e3d2c8be143cbbf10d6be1c2dae1
ITWaves-Nikhil <nikhil.itwaves@gmail.com>
2024-01-02 17:31:32 +0530

    initial commit

---
+++ src/constants/theme.js
@@ -0,0 +1,12 @@
+export const COLORS = {
+  primarbgdark: '#0f0f0f',
+  accent_red: '#fe0000',
+  inactive_grey: '#272727',
+  active_white: '#f0f1f0',
+  blue: '#3ea6fe',
+};
+export const FONTS = {
+  Regular: 'Roboto Regular',
+  Medium: 'Roboto Medium',
+  Bold: 'Roboto Bold',
+};
